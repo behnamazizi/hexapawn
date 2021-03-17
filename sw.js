d64b96aeb45a85f2823e70743dc01894f377ff0b
Behnam Azizi <bh.azizi@gmail.com>
2021-03-18 00:45:37 +0330

    Update sw.js

--- sw.js
+++ sw.js
@@ -1,24 +1,46 @@
-self.addEventListener('install', (e) => {
-    e.waitUntil(
-        caches.open('v1.5').then((cache) => cache.addAll([
-            '/hexapawn/',
-            '/hexapawn/index.html',
-            '/hexapawn/css/style.css',
-            '/hexapawn/js/script.js',
-            '/hexapawn/img/icon48.png',
-            '/hexapawn/img/icon72.png',
-            '/hexapawn/img/icon96.png',
-            '/hexapawn/img/icon144.png',
-            '/hexapawn/img/icon168.png',
-            '/hexapawn/img/icon192.png',
-            '/hexapawn/img/icon256.png'
 
-        ]))
-    )
+
+const CacheName = '0_45_18_Mar_21';
+const assets = [
+    '/hexapawn/',
+    '/hexapawn/index.html',
+    '/hexapawn/css/style.css',
+    '/hexapawn/js/script.js',
+    '/hexapawn/img/icon48.png',
+    '/hexapawn/img/icon72.png',
+    '/hexapawn/img/icon96.png',
+    '/hexapawn/img/icon144.png',
+    '/hexapawn/img/icon168.png',
+    '/hexapawn/img/icon192.png',
+    '/hexapawn/img/icon256.png'
+];
+
+// install event
+self.addEventListener('install', evt => {
+  evt.waitUntil(
+    caches.open(CacheName).then((cache) => {
+      cache.addAll(assets);
+    })
+  );
 });
 
-self.addEventListener('fetch', (e) => {
-    e.respondWith(
-        caches.match(e.request).then((response) => response || fetch(e.request)),
-    );
+// activate event
+self.addEventListener('activate', evt => {
+  console.log('service worker activated');
+  evt.waitUntil(
+    caches.keys().then(keys => {
+      return Promise.all(keys
+        .filter(key => key !== CacheName)
+        .map(key => caches.delete(key))
+      );
+    }));
+});
+
+// fetch event
+self.addEventListener('fetch', evt => {
+  evt.respondWith(
+    caches.match(evt.request).then(cacheRes => {
+      return cacheRes || fetch(evt.request);
+    })
+  );
 });
